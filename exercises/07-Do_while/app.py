ec86d0962e655d40f6a11fd18e8d2b3c38ce8d9e
Daniel Martinez <d.martinez74@gmail.com>
2023-05-19 22:00:05 +0000

    done until 8

--- exercises/07-Do_while/app.py
+++ exercises/07-Do_while/app.py
@@ -1,2 +1,11 @@
 
 #Your code go here:
+number = 20
+while number >= 0:
+    if number == 0:
+        print("LIFTOFF")
+    elif number % 5 == 0:
+        print(str(number)+"!")
+    else:
+        print(number)
+    number -= 1
--- exercises/08-Delete_element/app.py
+++ exercises/08-Delete_element/app.py
@@ -1,9 +1,17 @@
-people = ['juan','ana','michelle','daniella','stefany','lucy','barak']
+people = ['juan', 'ana', 'michelle', 'daniella', 'stefany', 'lucy', 'barak']
 
-#Your code go here:
+# Your code go here:
+
+
 def deletePerson(person_name):
-    #Your code go here:
-    
+    # Your code go here:
+    new_people = []
+    for person in people:
+          if person != person_name:
+            new_people.append(person)
+    return new_people
+
+
 print(deletePerson("daniella"))
 print(deletePerson("juan"))
 print(deletePerson("emilio"))
